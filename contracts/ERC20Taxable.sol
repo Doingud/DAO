609a96f00b696dab05a0f80099e5753f731be9ec
lourens linde <65844331+lokithe5th@users.noreply.github.com>
2022-06-23 14:27:44 +0200

    Update contracts/ERC20Taxable.sol
    Co-authored-by: decadanse <87744721+decadanse@users.noreply.github.com>

--- contracts/ERC20Taxable.sol
+++ contracts/ERC20Taxable.sol
@@ -65,6 +65,7 @@ contract ERC20Taxable is Context, IERC20, IERC20Metadata {
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
+        owner = msg.sender;
     }
 
     modifier onlyAddress(address authorizedAddress) {
