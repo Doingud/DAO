247e145529deb6aca4b1fa7ae575cf08ac166761
decadanse <gertesset@gmail.com>
2022-07-04 20:54:40 +0300

    no need in IERC20

--- contracts/FXAMORxGuildToken.sol
+++ contracts/FXAMORxGuildToken.sol
@@ -7,10 +7,8 @@
 
 pragma solidity 0.8.14;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
- import "./utils/ERC20Base.sol";
+import "./utils/ERC20Base.sol";
 
 contract FXAMORxGuild is ERC20Base, Ownable {
 
