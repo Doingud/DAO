40ac29d4031a8c7a3dc3af34fe15372ef58c0d32
decadanse <gertesset@gmail.com>
2022-07-04 23:32:07 +0300

    controller base

---
+++ contracts/Controller.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @title Controller contract
+/// @author Daoism Systems Team
+/// @notice Controller contract controls the all of the deployed contracts of the guild
+
+contract Controller {
+
+    int256[] reportsWeight; // this is an array, which describes the amount of the weight of each report.(So the reports will later receive payments based on this weight)
+    mapping(uint => mapping(address => int256)) votes; // votes mapping(uint report => mapping(address voter => int256 vote))
+    mapping(uint => address[]) voters; // voters mapping(uint report => address [] voters)
+    int[] reportsVoting; // results of the vote for the report with specific id
+    mapping(address => bool) impactMakers;
+    
+    address public _owner;
+    address public AMORxGuild; 
+    address public FXAMORxGuild;
+    address public guild;
+
+    event Initialized(bool success, address owner, address AMORxGuild);
+
+    bool private _initialized;
+
+    error Unauthorized();
+    error EmptyArray();
+    error InvalidParameters();
+
+    /// Invalid balance to transfer. Needed `minRequired` but sent `amount`
+    /// @param sent sent amount.
+    /// @param minRequired minimum amount to send.
+    error InvalidAmount (uint256 sent, uint256 minRequired);
+
+    /// Invalid address. Needed address != address(0)
+    error AddressZero();
+
+    /// Invalid address to transfer. Needed `to` != msg.sender
+    error InvalidSender();
+
+    function init(
+        string memory name_, 
+        string memory symbol_, 
+        address initOwner_,
+        address AMORxGuild_,
+        address FXAMORxGuild_,
+        address guild_
+    ) external returns (bool) {
+        require(!_initialized, "Already initialized");
+
+        _transferOwnership(initOwner_);
+
+        _owner = initOwner_;
+        AMORxGuild = AMORxGuild_;
+        FXAMORxGuild = FXAMORxGuild_;
+        _name = name_;
+        _symbol = symbol_;
+        guild = guild_;
+
+        _initialized = true;
+        emit Initialized(_initialized, initOwner_, AMORxGuild_);
+        return true;
+    }
+
+    modifier onlyAddress(address authorizedAddress) {
+        if (msg.sender != authorizedAddress) {
+            revert Unauthorized();
+        }
+        _;
+    }
+
+    /// @notice allows to donate AMORxGuild tokens to the Guild
+    /// @param amount The amount to donate
+    // It automatically distributes tokens between Impact and 
+    // project polls(which are both multisigs and governed by the owners of dAMOR) 
+    // in the 80%-20% distribution. 10% of the tokens in the impact pool 
+    // are getting staked in the FXAMORxGuild tokens, which are going to be owned by the user.
+    function donate(uint256 amount) public {
+
+
+        IERC20(AMORxGuild).transfer(guild, amount);
+
+
+        uint256 FxGAmount = amount / 10; // FXAMORxGuild Amount = 10% of AMORxGuild, eg = AMORxGuildAmount * 100 / 10
+        // 10% of the tokens in the impact pool are getting staked in the FXAMORxGuild tokens, 
+        // which are going to be owned by the user.
+        IERC20(FXAMORxGuild).transfer(msg.sender, amount);
+
+    }
+
+}
