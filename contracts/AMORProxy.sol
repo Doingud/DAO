0ccf23ab2ff7677090332c2596c18527ea4fdca4
lourens linde <65844331+lokithe5th@users.noreply.github.com>
2022-07-07 10:40:42 +0200

    Update AMORProxy.sol

--- contracts/AMORProxy.sol
+++ contracts/AMORProxy.sol
@@ -19,7 +19,7 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 
-contract AmorProxy is Proxy, ERC1967Upgrade {
+contract AMORProxy is Proxy, ERC1967Upgrade {
     bool private _initialized;
 
     error Initialized();
