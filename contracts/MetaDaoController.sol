c0d3a0b209b8f21c36f41fb20086133eaa3171ea
lokithe5th <lourenslinde@gmail.com>
2022-07-18 13:47:07 +0200

    Updates to functions

--- contracts/MetaDaoController.sol
+++ contracts/MetaDaoController.sol
@@ -15,9 +15,10 @@
 pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 
-contract MetaDaoController {
+contract MetaDaoController is AccessControl {
 
     //  Array of addresses of Guilds
     mapping(address => uint256) public guildWeight;
@@ -25,9 +26,12 @@ contract MetaDaoController {
     uint256 public guildsTotalWeight;
 
     /// Donation distribution weights
-    uint256 constant internal WEIGHT_TOTAL = 100;
     uint256 internal operationsWeight;
     uint256 internal buildersWeight;
+
+    /// Time snapshot
+    uint256 public claimStart;
+    uint256 public claimDuration;
 
     /// Operations pool address
     address public operationsPool;
@@ -36,19 +40,50 @@ contract MetaDaoController {
     /// Clone Factory
     address public guildFactory;
 
+    /// Distribution logic
+    /// Is a distribution cycle underway
+    bool public inDistribution;
+
     /// AMOR token
     IERC20 public amorToken;
+
+    /// Roles
+    bytes32 public constant GUILD_ROLE = keccak256("GUILD");
+
+    /// Errors
+    error AlreadyDistributing();
 
     constructor(address _amor, address _cloneFactory) {
         amorToken = IERC20(_amor);
         guildFactory = _cloneFactory;
+        claimDuration = 5 days;
     }
 
-    /// @notice Updates the MetaDAOs accounting of AMOR
-    /// @param  amount uint256 representing the amount of AMORxGuild staked in a guild
-    function updatePool(uint256 amount) public {
-        guildWeight[msg.sender] += amount;
-        guildsTotalWeight += amount;
+    /// @notice Updates a guild's weight
+    /// @param  amount the amount of staked AMORxGuild in this guild
+    /// @return bool the guild's balance was updated successfully
+    function updateGuildWeight(uint256 newWeight) external onlyRole(GUILD_ROLE) returns (bool) {
+        if (claimStart + claimDuration && inDistribution) {
+            inDistribution = false;
+        }
+        /// If `distribute` is still in cooldown, or if the guild weight does not change
+        if (inDistribution || guildWeight[msg.sender] == newWeight) {
+            return false;
+        }
+        /// If the new weight is less than the current weight:
+        /// Decrease the total guilds weight by the difference between the old and the new weight
+        /// Update guildWeight
+        if (guildWeight[msg.sender] > newWeight) {
+            guildsTotalWeight -= (guildWeight[msg.sender] - newWeight);
+            guildWeight[msg.sender] = newWeight;
+            return true;
+        }
+        /// If not yet exited then newWeight > guildWeight
+        /// Increase the total guild weight by the difference and set guildWeight == newWeight
+        guildsTotalWeight += (newWeight - guildWeight[msg.sender]);
+        guildWeight[msg.sender] = newWeight;
+        return true;
+
     }
 
     /// @notice Explain to an end user what this does
@@ -64,7 +99,11 @@ contract MetaDaoController {
     /// @dev Explain to a developer any extra details
     /// @param amount Amount of AMOR to be distributed
     function distribute(uint256 amount) public {
-
+        if (inDistribution) {
+            revert AlreadyDistributing();
+        }
+        inDistribution = true;
+        claimStart = block.timestamp;
     }
 
     /// @notice Explain to an end user what this does
