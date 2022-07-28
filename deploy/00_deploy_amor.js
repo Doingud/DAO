d1bac7c9f964f111425af7092dd1f5c932a6b13a
decadanse <gertesset@gmail.com>
2022-07-28 12:43:15 +0300

    goerli network

--- deploy/00_deploy_amor.js
+++ deploy/00_deploy_amor.js
@@ -1,47 +1,3 @@
-// const deployFunction = async ({ getNamedAccounts, deployments }) => {
-//     const { deploy, execute } = deployments;
-//     const { root } = await getNamedAccounts();
-
-//     const defaulTaxRate = 0;
-//     const admin = '0xdd634602038eBf699581D34d6142a4FB5aa66Ff5';
-
-//     const { address: amorAddress } = await deploy("AMORToken", {
-//         from: root,
-//         log: true,
-//         gasLimit: process.env.GAS_LIMIT,
-//     });
-
-//     console.log("root is %s", root);
-//     console.log("admin is %s", admin);
-//     const AMOR = await ethers.getContract("AMORToken");
-
-//     const AMORDeployed = await AMOR.deploy();
-
-//     await AMORDeployed.init("AMOR Token", "AMOR", admin, defaulTaxRate, admin);
-
-// console.log("AMORDeployed is %s", AMORDeployed.address);
-
-//     // // console.log("tx is %s", tx);
-
-//     // // tx = await AMOR.init("AMOR Token", "AMOR", admin, defaulTaxRate, admin, {
-//     // //     from: admin,
-//     // //     gasLimit: process.env.GAS_LIMIT,
-//     // // });
-
-//     // await execute('AMORToken', {
-//     //     from: root, log: true, gasLimit: process.env.GAS_LIMIT,
-//     // }, 'init', "AMOR Token", "AMOR", admin, defaulTaxRate, admin)
-
-//     console.log('AMORToken: ', amorAddress);
-//     // // await execute('AMORToken', { from: root, log: true, gasLimit: process.env.GAS_LIMIT }, 'init', "AMOR Token", "AMOR", admin, defaulTaxRate, admin)
-//     // // await AMOR.init("AMOR Token", "AMOR", addresses.taxController, defaulTaxRate, addresses.multisig);
-//     // console.log("tx is %s", tx);
-// };
-
-// module.exports = deployFunction;
-// module.exports.tags = ["AMORToken"];
-
-
 async function main() {
     const [deployer] = await ethers.getSigners();
     const admin = '0xdd634602038eBf699581D34d6142a4FB5aa66Ff5';
@@ -55,7 +11,7 @@ async function main() {
 
     const tx = await AMOR.init("AMOR Token", "AMOR", admin, defaulTaxRate, admin);
     console.log("tx is %s", tx);
-    console.log("Token address:", AMOR.address);
+    console.log("AMOR address:", AMOR.address);
   }
   
   main()
