3e00c3a3197c892a1cb1b48590f79ca8c3f56bf7
lourens linde <65844331+lokithe5th@users.noreply.github.com>
2022-07-29 08:45:20 +0200

    ignore this change

--- test/unit/guildcontroller.js
+++ test/unit/guildcontroller.js
@@ -1,4 +1,3 @@
-const { time } = require("@openzeppelin/test-helpers");
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const { ONE_HUNDRED_ETHER,
@@ -9,9 +8,7 @@ const { ONE_HUNDRED_ETHER,
 const init = require('../test-init.js');
 
 const FEE_DENOMINATOR = 1000;
-const percentToConvert = 100; //10% // FEE_DENOMINATOR/100*10
-const averageLockTime = time.duration.days(7);
-const twoWeeks = time.duration.days(14);
+const percentToConvert = 100; //10% // (FEE_DENOMINATOR*10)/100
 
 const TEST_TRANSFER_SMALLER = 80;
 
@@ -20,7 +17,6 @@ let AMORxGuild;
 let FXAMORxGuild
 let controller;
 let root;
-let authorizer_adaptor;
 let impactMaker;
 let operator;
 let user;
@@ -28,10 +24,6 @@ let user2;
 let staker;
 let impactMakers;
 let weigths;
-let report;
-let r;
-let s;
-let v;
 
 describe('unit - Contract: GuildController', function () {
 
@@ -50,7 +42,6 @@ describe('unit - Contract: GuildController', function () {
         impactMaker = setup.roles.doingud_multisig;
         user = setup.roles.user3;
         user2 = setup.roles.user2;
-        authorizer_adaptor = setup.roles.authorizer_adaptor;
         operator = setup.roles.operator;
     });
 
@@ -164,8 +155,8 @@ describe('unit - Contract: GuildController', function () {
 
             await AMOR.connect(user).approve(AMORxGuild.address, TEST_TRANSFER);
 
-            let AMORDeducted = ethers.BigNumber.from((TEST_TRANSFER*(BASIS_POINTS-TAX_RATE)/BASIS_POINTS).toString());
-            let nextAMORDeducted =  ethers.BigNumber.from((AMORDeducted*(BASIS_POINTS-TAX_RATE)/BASIS_POINTS).toString());
+            let AMORDeducted = ethers.BigNumber.from((TEST_TRANSFER*(BASIS_POINTS-TAX_RATE)/BASIS_POINTS).toString());//Math.ceil(TEST_TRANSFER * (1 - TAX_RATE / BASIS_POINTS));
+            let nextAMORDeducted =  ethers.BigNumber.from((AMORDeducted*(BASIS_POINTS-TAX_RATE)/BASIS_POINTS).toString());//Math.ceil(AMORDeducted * (1 - TAX_RATE / BASIS_POINTS));
 
             await AMORxGuild.connect(user).stakeAmor(user.address, nextAMORDeducted);
             await AMORxGuild.connect(user).approve(controller.address, nextAMORDeducted);
@@ -213,173 +204,6 @@ describe('unit - Contract: GuildController', function () {
             const balanceAfter = await controller.claimableTokens(impactMaker.address);
             await controller.connect(impactMaker).claim(impactMaker.address);
             expect((await AMORxGuild.balanceOf(impactMaker.address)).toString()).to.equal(balanceAfter.toString());
-        });
-    });
-
-    context('» addReport testing', () => {
-
-        it('it fails to add report if Unauthorized', async function () {
-            await AMORxGuild.connect(operator).approve(controller.address, TEST_TRANSFER);
-
-            const timestamp = Date.now();
-
-            // building hash has to come from system address
-            // 32 bytes of data
-            let messageHash = ethers.utils.solidityKeccak256(
-                ["address", "uint", "string"],
-                [operator.address, timestamp, "report info"]
-            );
-
-            // 32 bytes of data in Uint8Array
-            let messageHashBinary = ethers.utils.arrayify(messageHash);
-            
-            // To sign the 32 bytes of data, make sure you pass in the data
-            let signature = await operator.signMessage(messageHashBinary);
-
-            // split signature
-            r = signature.slice(0, 66);
-            s = "0x" + signature.slice(66, 130);
-            v = parseInt(signature.slice(130, 132), 16);
-                    
-            report = messageHash;
-            await expect(controller.connect(authorizer_adaptor).addReport(report, v, r, s)).to.be.revertedWith(
-                'Unauthorized()'
-            );
-        });
-
-        it('adds report', async function () {
-            await controller.connect(operator).addReport(report, v, r, s); 
-            expect((await controller.queueReportsAuthors(0))).to.equal(operator.address);
-        });
-
-    });
-
-    context('» startVoting testing', () => {
-
-        it('it fails to finalize report voting if trigger in false state', async function () {
-            expect(await controller.trigger()).to.equal(false);
-            await expect(controller.connect(authorizer_adaptor).finalizeVoting()).to.be.revertedWith(
-                'ReportNotExists()'
-            );
-        });
-
-        it('it fails to start report voting if InvalidAmount of the reports in queue', async function () {
-            await expect(controller.connect(root).startVoting()).to.be.revertedWith(
-                'InvalidAmount()'
-            );
-        });
-
-        it('it starts reports voting', async function () {
-            await AMORxGuild.connect(operator).approve(controller.address, TEST_TRANSFER);
-
-            // add 9 more reports
-            await controller.connect(operator).addReport(report, v, r, s); // 1
-            await controller.connect(operator).addReport(report, v, r, s); // 2
-            await controller.connect(operator).addReport(report, v, r, s); // 3
-            await controller.connect(operator).addReport(report, v, r, s); // 4
-            await controller.connect(operator).addReport(report, v, r, s); // 5
-            await controller.connect(operator).addReport(report, v, r, s); // 6
-            await controller.connect(operator).addReport(report, v, r, s); // 7
-            await controller.connect(operator).addReport(report, v, r, s); // 8
-            await controller.connect(operator).addReport(report, v, r, s); // 9
-            expect((await controller.queueReportsAuthors(9))).to.equal(operator.address);
-
-            await controller.connect(authorizer_adaptor).startVoting();
-
-            expect(await controller.trigger()).to.equal(true);
-            await expect(controller.queueReportsAuthors()).to.be.reverted;
-            expect((await controller.reportsAuthors(9))).to.equal(operator.address);
-        });
-
-        it('it fails to start report voting if VotingTimeNotFinished', async function () {
-            await expect(controller.connect(authorizer_adaptor).startVoting()).to.be.revertedWith(
-                'VotingTimeNotFinished()'
-            );
-        });
-    });
-
-    context('» voteForReport testing', () => {
-
-        it('it fails to vote for report if ReportNotExists', async function () {
-            await AMORxGuild.connect(operator).approve(controller.address, TEST_TRANSFER);
-            const id = 11;
-            const amount = 12;
-            const sign = true;            
-            await expect(controller.connect(authorizer_adaptor).voteForReport(id, amount, sign)).to.be.revertedWith(
-                'ReportNotExists()'
-            );
-        });
-
-        it('votes for report positivly', async function () {
-            await AMOR.connect(root).transfer(controller.address, TEST_TRANSFER);
-            await AMOR.connect(root).transfer(operator.address, TEST_TRANSFER);
-            await AMOR.connect(operator).approve(AMORxGuild.address, TEST_TRANSFER);
-            let AMORDeducted = ethers.BigNumber.from((TEST_TRANSFER*(BASIS_POINTS-TAX_RATE)/BASIS_POINTS).toString());
-            let nextAMORDeducted =  ethers.BigNumber.from((AMORDeducted*(BASIS_POINTS-TAX_RATE)/BASIS_POINTS).toString());
-            await AMORxGuild.connect(operator).stakeAmor(operator.address, nextAMORDeducted);
-            await AMORxGuild.connect(operator).approve(controller.address, nextAMORDeducted);
-            await controller.connect(operator).donate(TEST_TRANSFER_SMALLER);        
-
-            const id = 0;
-            const amount = 2;
-            const sign = true;
-            await controller.connect(user).voteForReport(id, amount, sign);
-            expect(await controller.reportsVoting(0)).to.equals(2);
-            expect(await controller.reportsWeight(0)).to.equals(2);
-
-            await controller.connect(operator).voteForReport(id, 1, false);
-            expect(await controller.reportsVoting(0)).to.equals(1);
-            expect(await controller.reportsWeight(0)).to.equals(3);
-        });
-
-        it('votes for report negatively', async function () {
-            const id = 1;
-            const amount = 2;
-            const sign = false;
-            await controller.connect(operator).voteForReport(id, amount, sign);
-            expect(await controller.reportsVoting(1)).to.equals(-2);
-            expect(await controller.reportsWeight(1)).to.equals(2);
-
-            await controller.connect(user).voteForReport(id, 1, true);
-        });
-
-        it('it fails to vote for report if try to vote more than user have', async function () {
-            const id = 0;
-            const amount = TEST_TRANSFER;
-            const sign = true;
-            await expect(controller.connect(authorizer_adaptor).voteForReport(id, amount, sign)).to.be.revertedWith(
-                'InvalidAmount()'
-            );
-        });
-
-        it('it fails to finalize report voting if VotingTimeNotFinished', async function () {
-            expect(await controller.trigger()).to.equal(true);
-            await expect(controller.connect(authorizer_adaptor).finalizeVoting()).to.be.revertedWith(
-                'VotingTimeNotFinished()'
-            );
-        });
-
-        it('it fails to vote for report if VotingTimeExpired', async function () {
-            const id = 0;
-            const amount = 2;
-            const sign = true;
-            await controller.connect(operator).voteForReport(id, amount, sign);
-            time.increase(averageLockTime);
-
-            await expect(controller.connect(operator).voteForReport(id, amount, sign)).to.be.revertedWith(
-                'VotingTimeExpired()'
-            );
-        });
-    });
-
-    context('» finalizeVoting testing', () => {
-
-        it('finalizes voting', async function () {
-            const balanceBefore = await AMORxGuild.balanceOf(operator.address);
-            time.increase(twoWeeks);
-            await controller.connect(operator).finalizeVoting();
-            const balanceAfter = balanceBefore.add(2);
-            expect((await AMORxGuild.balanceOf(operator.address)).toString()).to.equal(balanceAfter.toString());
         });
     });
 });
