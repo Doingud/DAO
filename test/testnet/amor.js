4de4a257137d536cd4e4717c1e88a7ac9aad5cd9
decadanse <gertesset@gmail.com>
2022-08-04 13:18:15 +0300

    goerli - AMOR Token test commented

--- test/testnet/amor.js
+++ test/testnet/amor.js
@@ -1,22 +1,22 @@
-const { expect } = require('chai');
-const { getAddresses, multisig, AMOR } = require('../../config');
-const { getContract } = require('../helpers/helpers');
-const addresses = getAddresses();
+// const { expect } = require('chai');
+// const { getAddresses, multisig, AMOR } = require('../../config');
+// const { getContract } = require('../helpers/helpers');
+// const addresses = getAddresses();
 
-const MULTISIG = addresses[multisig];
-const amor = addresses[AMOR];
+// const MULTISIG = addresses[multisig];
+// const amor = addresses[AMOR];
 
-describe("goerli - AMOR Token", function () {
+// describe("goerli - AMOR Token", function () {
 
-    context('» init testing', () => {
-        it('initialized variables check', async function () {
-            const AMORToken = await getContract('AMORToken', amor);
+//     context('» init testing', () => {
+//         it('initialized variables check', async function () {
+//             const AMORToken = await getContract('AMORToken', amor);
 
-            expect(await AMORToken.name()).to.equals("AMOR Token");
-            expect(await AMORToken.symbol()).to.equals("AMOR");
-            expect(await AMORToken.taxController()).to.equals(MULTISIG);
-            expect(await AMORToken.owner()).to.equals(MULTISIG);
-        });
-    });
-});
+//             expect(await AMORToken.name()).to.equals("AMOR Token");
+//             expect(await AMORToken.symbol()).to.equals("AMOR");
+//             expect(await AMORToken.taxController()).to.equals(MULTISIG);
+//             expect(await AMORToken.owner()).to.equals(MULTISIG);
+//         });
+//     });
+// });
 
