32602607f9c1258746dee83992f11566329e6e5e
SaadmanM <saadman.masud15@gmail.com>
2022-08-18 20:23:55 +0700

    Update ICloneFactory.sol

--- contracts/utils/interfaces/ICloneFactory.sol
+++ contracts/utils/interfaces/ICloneFactory.sol
@@ -11,9 +11,5 @@ interface ICloneFactory {
 
     error ArrayMismatch();
 
-    function deployGuildContracts(
-        address owner,
-        string memory _name,
-        string memory _symbol
-    ) external;
+    function deployGuildContracts(string memory _name, string memory _symbol) external;
 }
