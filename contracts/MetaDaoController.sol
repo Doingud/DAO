105a6d07a334d3f25899be1c839729aa4ca184e3
lokithe5th <lourenslinde@gmail.com>
2022-08-29 13:18:40 +0200

    Update MetaDaoController.sol
    Removed:
    - distributeToken, distributeTokens
    - addGuild
    - updateGuildWeights
    
    Added:
    - claimToken, claimTokens
    
    Modified:
    - donate
    - addIndex

--- contracts/MetaDaoController.sol
+++ contracts/MetaDaoController.sol
@@ -55,6 +55,7 @@ contract MetaDaoController is AccessControl {
     /// Create an array to hold the different indexes
     mapping(bytes32 => Index) public indexes;
     bytes32[] public indexHashes;
+    bytes32 public constant FEES_INDEX = keccak256("FEES_INDEX");
 
     /// Errors
     /// The claim is not valid
@@ -85,28 +86,15 @@ contract MetaDaoController is AccessControl {
         whitelist[SENTINEL] = _amor;
     }
 
-    /// @notice Updates a guild's weight
-    /// @param  guildArray addresses of the guilds
-    /// @param  newWeights weights of the guilds, must be correspond to the order of `guildArray`
-    function updateGuildWeights(address[] memory guildArray, uint256[] memory newWeights)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        if (guildArray.length != guilds.length || newWeights.length != guilds.length) {
-            revert InvalidGuild();
-        }
-        guildsTotalWeight = 0;
-        for (uint256 i; i < guildArray.length; i++) {
-            guildWeight[guildArray[i]] = newWeights[i];
-            guildsTotalWeight += newWeights[i];
-        }
-    }
-
     /// @notice Allows a user to donate a whitelisted asset
     /// @dev    `approve` must have been called on the `token` contract
     /// @param  token the address of the token to be donated
     /// @param  amount the amount of tokens to donate
-    function donate(address token, uint256 amount) external {
+    function donate(
+        address token,
+        uint256 amount,
+        uint256 index
+    ) external {
         if (whitelist[token] == address(0)) {
             revert NotListed();
         }
@@ -114,61 +102,69 @@ contract MetaDaoController is AccessControl {
             uint256 amorBalance = amorToken.balanceOf(address(this));
             IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
             amorBalance = amorToken.balanceOf(address(this)) - amorBalance;
+            allocateByIndex(token, amorBalance, index);
             donations[token] += amorBalance;
         } else {
             donations[token] += amount;
             IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+            allocateByIndex(token, amount, index);
+        }
+    }
+
+    /// @notice Allocates donated funds by the index specified
+    /// @dev    This approach allows any guild to claim their funds at any time
+    /// @param  token address of the ERC20 token to be donated
+    /// @param  amount of the specified token to be allocated
+    /// @param  index the index to be used to allocate the donation by
+    function allocateByIndex(
+        address token,
+        uint256 amount,
+        uint256 index
+    ) internal {
+        Index memory index = indexes[indexHashes[index]];
+        for (uint256 i = 0; i < guilds.length; i++) {
+            uint256 amountAllocated = (amount * index.indexWeights[guilds[i]]) / index.indexDenominator;
+            guildFunds[guilds[i]][token] += amountAllocated;
         }
     }
 
     /// @notice Distributes both the fees and the token donations
-    function distributeAll() external {
+    function claimAll(address guild) external {
         /// Apportion the AMOR received from fees
-        distributeFees(index);
+        distributeFees();
         /// Apportion the token donations
-        distributeTokens(index);
+        claimTokens(guild);
     }
 
     /// @notice Distributes the specified token
     /// @param  token address of target token
-    function distributeToken(address token) public {
+    function claimToken(address token, address guild) public {
         if (whitelist[token] == address(0)) {
             revert NotListed();
         }
-        uint256 trackDistributions;
-        /// Loop through guilds
-        for (uint256 i = 0; i < guilds.length; i++) {
-            if (guildWeight[guilds[i]] == 0) {
-                revert ArrayError({guild: guilds[i], index: i});
-            }
-            uint256 amountToDistribute = (donations[token] * guildWeight[guilds[i]]) / guildsTotalWeight;
-            if (amountToDistribute == 0) {
-                continue;
-            }
-            guildFunds[guilds[i]][token] += amountToDistribute;
-            /// Update the donation total for this token
-            trackDistributions += amountToDistribute;
-        }
-        /// Decrease the donations by the amount of tokens distributed
-        /// This prevents multiple calls to distribute attack vector
-        donations[token] -= trackDistributions;
+        uint256 amount = guildFunds[guild][token];
+        donations[token] -= amount;
+        /// Clear this guild's token balance
+        delete guildFunds[guild][token];
+        IERC20(whitelist[helper]).safeTransfer(guild, amount);
     }
 
     /// @notice Apportions approved token donations according to guild weights
     /// @dev    Loops through all whitelisted tokens and calls `distributeToken()` for each
-    function distributeTokens() public {
+    /// @param  guild the address of the guild claiming tokens
+    function claimTokens(address guild) public onlyRole(GUILD_ROLE) {
         address endOfList = SENTINEL;
         /// Loop through linked list
         while (whitelist[endOfList] != SENTINEL) {
-            distributeToken(whitelist[endOfList]);
+            claimToken(whitelist[endOfList], guild);
             endOfList = whitelist[endOfList];
         }
     }
 
     /// @notice Apportions collected AMOR fees
     /// @param  index the index to use
-    function distributeFees(uint256 index) internal {
-        Index memory index = indexes[indexHashes[index]];
+    function distributeFees() internal {
+        Index memory index = indexes[feesIndex];
         /// Determine amount of AMOR that has been collected from fees
         uint256 feesToBeDistributed = amorToken.balanceOf(address(this)) - donations[address(amorToken)];
         for (uint256 i = 0; i < guilds.length; i++) {
@@ -176,21 +172,6 @@ contract MetaDaoController is AccessControl {
             if (amountToDistribute != 0) {
                 guildFunds[guilds[i]][address(amorToken)] += amountToDistribute;
             }
-        }
-    }
-
-    /// @notice Transfers apportioned tokens from the metadao to the guild
-    /// @dev only a guild can call this funtion
-    function claim() public onlyRole(GUILD_ROLE) {
-        /// Loop through the token linked list
-        address helper = SENTINEL;
-        while (whitelist[helper] != SENTINEL) {
-            /// Donate or transfer the token
-            IERC20(whitelist[helper]).safeTransfer(msg.sender, guildFunds[msg.sender][whitelist[helper]]);
-            /// Clear this guild's token balance
-            delete guildFunds[msg.sender][whitelist[helper]];
-            /// Advance the helper to the next link in the list
-            helper = whitelist[helper];
         }
     }
 
@@ -204,14 +185,7 @@ contract MetaDaoController is AccessControl {
         string memory name,
         string memory tokenSymbol
     ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        address newGuildController = ICloneFactory(guildFactory).deployGuildContracts(guildOwner, name, tokenSymbol);
-        addGuild(newGuildController);
-    }
-
-    /// @notice adds guild based on the controller address provided
-    /// @dev    give guild role in access control to the controller for the guild
-    /// @param  controller the controller address of the guild
-    function addGuild(address controller) internal {
+        address controller = ICloneFactory(guildFactory).deployGuildContracts(guildOwner, name, tokenSymbol);
         grantRole(GUILD_ROLE, controller);
         guilds.push(controller);
     }
