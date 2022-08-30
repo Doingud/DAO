16549459ed5449e75dcfc8962938bbfcd0a0848b
decadanse <gertesset@gmail.com>
2022-08-30 19:43:30 +0300

    claim array of tokens

--- test/unit/guildcontroller.js
+++ test/unit/guildcontroller.js
@@ -339,8 +339,13 @@ describe('unit - Contract: GuildController', function () {
             expect(balanceBefore).to.equal(0);
 
             const balanceAfter = await controller.claimableTokens(impactMaker.address, AMORxGuild.address);
-            await controller.connect(impactMaker).claim(impactMaker.address, [AMORxGuild.address]);
+            // check that passing empty-claimable token in array would cause no errors
+            expect((await controller.claimableTokens(impactMaker.address, FXAMORxGuild.address)).toString()).to.equal("0");
+            expect((await FXAMORxGuild.balanceOf(impactMaker.address)).toString()).to.equal("0");
+
+            await controller.connect(impactMaker).claim(impactMaker.address, [AMORxGuild.address, FXAMORxGuild.address]);
             expect((await AMORxGuild.balanceOf(impactMaker.address)).toString()).to.equal(balanceAfter.toString());
+            expect((await FXAMORxGuild.balanceOf(impactMaker.address)).toString()).to.equal("0");
         });
     });
 
