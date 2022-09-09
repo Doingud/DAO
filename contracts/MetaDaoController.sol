6131c0fe52a3ddf28b962a01460762474809c8ef
lourens linde <65844331+lokithe5th@users.noreply.github.com>
2022-09-09 13:20:18 +0200

    Update MetaDaoController.sol
    Natspec and licensing update

--- contracts/MetaDaoController.sol
+++ contracts/MetaDaoController.sol
@@ -1,17 +1,38 @@
 // SPDX-License-Identifier: MIT
-
-/// @title  MetaDAO Controller
-/// @author Daoism Systems Team
-/// @custom security-contact contact@daoism.systems
+pragma solidity 0.8.15;
 
 /**
- *  @dev Implementation of the MetaDAO controller logic for DoinGud MetaDAO
+ * @title  DoinGud: MetaDaoController.sol
+ * @author Daoism Systems
+ * @notice MetaDaoController implementation for DoinGudDAO
+ * @custom Security-contact arseny@daoism.systems || konstantin@daoism.systems
  *
  *  The MetaDAO creates new guilds and collects fees from AMOR token transfers.
- *  The collected funds can then be claimed by guilds.
+ *  The collected funds can then be distributed and claimed by guilds.
+ *
+ * MIT License
+ * ===========
+ *
+ * Copyright (c) 2022 DoinGud
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *
  */
-
-pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
