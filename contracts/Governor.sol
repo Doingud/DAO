cde1d612c9e18e441fee1b5d9000e65291ee8c09
decadanse <gertesset@gmail.com>
2022-10-06 21:22:42 +0300

    updeted

--- contracts/Governor.sol
+++ contracts/Governor.sol
@@ -84,7 +84,6 @@ contract DoinGudGovernor {
     address public snapshotAddress;
     address public avatarAddress;
     IERC20 private AMORxGuild;
-    IERC20 public token;
 
     event Initialized(bool success, address avatarAddress, address snapshotAddress);
     event ProposalCreated(
@@ -128,7 +127,6 @@ contract DoinGudGovernor {
         if (_initialized) {
             revert AlreadyInitialized();
         }
-        token = IERC20(AMORxGuild_);
         _name = name;
         // person who inflicted the creation of the contract is set as the only guardian of the system
         guardians.push(msg.sender);
