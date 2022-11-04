e40aa533b247f5002cbb6e833cb14106aa7c6a2b
decadanse <87744721+decadanse@users.noreply.github.com>
2022-11-04 15:00:09 +0300

    Update contracts/GuildFactory.sol
    Co-authored-by: lourens linde <65844331+lokithe5th@users.noreply.github.com>

--- contracts/GuildFactory.sol
+++ contracts/GuildFactory.sol
@@ -64,7 +64,7 @@ contract GuildFactory is ICloneFactory, Ownable {
     uint256 public constant DEFAULT_GUARDIAN_THRESHOLD = 10;
 
     /// EVENTS
-    event guildContractsCreated(
+    event GuildContractsCreated(
         address GuildController,
         address AmorGuildToken,
         address DAmorxGuild,
